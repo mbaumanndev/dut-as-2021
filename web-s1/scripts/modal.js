1b28894613fbb3832ab7be3ff540f127f4aaa734
Maxime BAUMANN <499268+mbaumanndev@users.noreply.github.com>
2021-12-03 09:10:08 +0100

    Add files via upload

--- web-s1/scripts/modal.js
+++ web-s1/scripts/modal.js
@@ -1,5 +1,6 @@
 const openBtn = document.getElementById('open-modal')
 
+/*
 openBtn.addEventListener('click', function click(ev) {
     // On créée la modale
     const modal = document.createElement("div")
@@ -30,6 +31,64 @@ openBtn.addEventListener('click', function click(ev) {
     confirm.setAttribute('type', 'button')
     confirm.innerText = "Confirmer"
 
+    function closeModal() {
+        close.removeEventListener('click', closeModal)
+        cancel.removeEventListener('click', closeModal)
+        confirm.removeEventListener('click', closeModal)
+        modal.remove()
+    }
+
+    close.addEventListener('click', closeModal)
+    cancel.addEventListener('click', closeModal)
+    confirm.addEventListener('click', closeModal)
+
     // On l'ajoute au document
     document.body.appendChild(modal)
 })
+*/
+
+/*
+const modal = document.getElementById('modal')
+openBtn.addEventListener('click', function click() {
+    modal.style.display = 'block'
+
+    const close = modal.querySelector('.close')
+    const cancel = modal.querySelector('.cancel')
+    const confirm = modal.querySelector('.confirm')
+
+    function closeModal() {
+        close.removeEventListener('click', closeModal)
+        cancel.removeEventListener('click', closeModal)
+        confirm.removeEventListener('click', closeModal)
+        modal.style.display = 'none'
+    }
+
+    close.addEventListener('click', closeModal)
+    cancel.addEventListener('click', closeModal)
+    confirm.addEventListener('click', closeModal)
+})
+*/
+
+const template = document.getElementById('modalTemplate')
+openBtn.addEventListener('click', function click() {
+    const clone = template.content.cloneNode(true)
+    const modal = clone.querySelector('.modal')
+
+    const close = modal.querySelector('.close')
+    const cancel = modal.querySelector('.cancel')
+    const confirm = modal.querySelector('.confirm')
+
+    function closeModal() {
+        close.removeEventListener('click', closeModal)
+        cancel.removeEventListener('click', closeModal)
+        confirm.removeEventListener('click', closeModal)
+        modal.remove()
+    }
+
+    close.addEventListener('click', closeModal)
+    cancel.addEventListener('click', closeModal)
+    confirm.addEventListener('click', closeModal)
+
+    document.body.appendChild(modal)
+})
+
