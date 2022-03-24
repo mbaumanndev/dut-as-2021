2a799afcf404e6a933edd7b4060d37999294f37b
Maxime BAUMANN <499268+mbaumanndev@users.noreply.github.com>
2022-03-24 10:01:24 +0100

    Add files via upload

--- cours-web-s2/Controllers/MoviesController.cs
+++ cours-web-s2/Controllers/MoviesController.cs
@@ -21,9 +21,16 @@ namespace Iut.Demo.Web.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            var movies = _context.Movie.AsQueryable();
+
+            if (!String.IsNullOrWhiteSpace(searchString))
+            {
+                movies = movies.Where(m => m.Title!.Contains(searchString));
+            }
+
+            return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
